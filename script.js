d8ea1e7e1a56f9341e2e18e84fe762705dc65344
kambleps <125243027+kambleps@users.noreply.github.com>
2023-05-19 19:19:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let display=document.getElementById("counter");
+let increment=document.getElementById("increment");
+let count=0;
+display.innerHTML=count;
+let handleIncrement=()=>{
+	count++;
+	display.innerHTML=count;
+}
+increment.addEventListener("click",handleIncrement);
+
