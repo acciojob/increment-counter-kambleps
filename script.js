a52bd67c9de2c36d09ce5bbb3276e6fe8cec2647
kambleps <125243027+kambleps@users.noreply.github.com>
2023-05-19 19:25:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 let display=document.getElementById("counter");
-let increment=document.getElementById("increment");
+let increment=document.getElementById("incrementBtn");
 let count=0;
 display.innerHTML=count;
 let handleIncrement=()=>{
